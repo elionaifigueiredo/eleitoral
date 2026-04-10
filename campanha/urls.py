89d2dcdff690b2ab6298bd3bb4cf42adde5bfcdb
Elionai Figueiredo <elionaiesf@adv.net>
2026-04-10 17:34:34 -0300

    adicionar melhorias

--- campanha/urls.py
+++ campanha/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
+
+path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 
     path('login/', auth_views.LoginView.as_view(template_name='campanha/login.html'), name='login'),
